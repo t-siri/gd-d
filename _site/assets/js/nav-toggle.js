e0dbf5cda5e157f8bfdf5e98dd84b4cb783bae10
Toy Sirichindakul <toy@Toys-MacBook-Pro.local>
2021-05-29 10:50:09 +1000

    nav fix

--- _site/assets/js/nav-toggle.js
+++ _site/assets/js/nav-toggle.js
@@ -2,7 +2,6 @@ function toggleMobNav() {
 	var burger = document.getElementById("menu-link");
 	var navicon = document.getElementById("nav-icon1");
 	var nav = document.getElementById("navigation");
-	var contact = document.getElementById("contact");
 	var html = document.getElementsByTagName("html")[0];
 	burger.addEventListener('click', function() {
 		burger.classList.toggle('active');
--- assets/js/nav-toggle.js
+++ assets/js/nav-toggle.js
@@ -1,29 +1,7 @@
-function toggleContact() {
-	var contact = document.getElementById('contact-content');
-	contact.classList.toggle('contact-content__visible');
-	contact.classList.toggle('contact-content__hidden');
-}
-
-function toggleContactX() {
-	var contact = document.getElementById('contact-content');
-	var close = document.getElementById('close-nav');
-	var formClose = document.getElementById('form-close');
-	close.addEventListener('click', function() {
-		contact.classList.toggle('contact-content__visible');
-		contact.classList.toggle('contact-content__hidden');
-	});
-	formClose.addEventListener('click', function() {
-		contact.classList.toggle('contact-content__visible');
-		contact.classList.toggle('contact-content__hidden');
-	});
-}
-toggleContactX() 
-
 function toggleMobNav() {
 	var burger = document.getElementById("menu-link");
 	var navicon = document.getElementById("nav-icon1");
 	var nav = document.getElementById("navigation");
-	var contact = document.getElementById("contact");
 	var html = document.getElementsByTagName("html")[0];
 	burger.addEventListener('click', function() {
 		burger.classList.toggle('active');
